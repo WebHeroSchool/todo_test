81758e4dbb5a57088cb03ae2aba8ba52a342bd93
Анищик Анастасия <an.anischik@gmail.com>
2020-06-09 01:01:33 +0300

    task

--- src/App.js
+++ src/App.js
@@ -1,45 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-const variable = 'Hello';
-const logic = true;
-const tern = false;
+import { count, length } from "./number";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 style={{
-          color: 'grey',
-          fontSize: 35
-        }}>
-          First app</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {variable}
-        </p>
-        <p>
-          Number {4}
-        </p>
-        <p>
-          Number {1+3}
-        </p>
-        <p>
-          {logic && 'Logic is true'}
-        </p>
-        <p>
-          {tern ? 'Tern is true' : 'Tern is false'}
-        </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>
-          {undefined}
-          {null}
-          {false}
-          {true}
-        </p>
+        <p>Результат: {count * length}</p>
         <a
           className="App-link"
           href="https://reactjs.org"

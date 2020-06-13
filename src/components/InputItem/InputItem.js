820e2c36aa9000d738befa437a1cf364a7f70951
Анищик Анастасия <an.anischik@gmail.com>
2020-06-13 16:55:00 +0300

    added material-UI

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 
-const InputItem = () => (
-    <div>
-        <input type="text"/>
-    </div>);
+const InputItem = () => (<div>
+    <TextField 
+        id="standard-search" 
+        label="Выбрать дело" 
+        type="search" 
+    />
+</div>);
 
 export default InputItem;

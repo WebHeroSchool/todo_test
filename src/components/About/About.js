5694fa0c1a3ecf550d102f3954c876bdc3ec02ad
Анищик Анастасия <an.anischik@gmail.com>
2020-06-27 02:34:41 +0300

    corr style

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -29,7 +29,7 @@ class About extends React.Component {
                 isLoading: false,
                 isError: true,
                 error: error.message
-            })
+            });
           });
 
         octokit.users.getByUsername({
